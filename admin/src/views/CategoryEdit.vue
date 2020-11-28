e954947c4bc0b4b57287e3bfb73fc077563f9f18
TUTWO <2395650867@qq.com>
2020-11-28 15:23:24 +0800

    新建，查询，编辑，删除分类

--- admin/src/views/CategoryEdit.vue
+++ admin/src/views/CategoryEdit.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="about">
-        <h1>新建分类</h1>
+        <h1>{{id ? "编辑" : "新建"}}分类</h1>
         <el-form label-width="100px" @submit.native.prevent="save">
+            <el-form-item label="上级分类">
+                <el-select v-model="model.parent">
+                    <el-option v-for="parent in parents" :key="parent._id" :label="parent.name" :value="parent._id"></el-option>
+                </el-select>
+            </el-form-item>
             <el-form-item label="名称">
                 <el-input v-model="model.name"></el-input>
             </el-form-item>
@@ -13,19 +18,42 @@
 </template>
 <script>
 export default {
+    props: {
+        id: {}
+    },
     data() {
         return {
-            model: {}
+            model: {},
+            parents: [],
         } 
+    },
+    created() {
+        this.fetchParents()
+        this.id && this.fetch()
     },
     methods: {
         async save() {
-            const res = await this.$http.post('categries', this.model)
-            this.$router.push('/categries/list')
-            this.$message({
-                type: 'success',
-                message: '保存成功!'
-            })
+            let res
+            if (this.id) {
+                res = await this.$http.put(`categories/${this.id}`, this.model)
+            } else {
+                res = await this.$http.post('categories', this.model)
+            }
+            if (res.data) {
+                this.$message({
+                    type: 'success',
+                    message: '保存成功!'
+                })
+                this.$router.push('/categories/list')
+            }
+        },
+        async fetch() {
+            const res = await this.$http.get(`categories/${this.id}`)
+            this.model = res.data
+        },
+        async fetchParents() {
+            const res = await this.$http.get(`categories`)
+            this.parents = res.data
         }
     }
 }
